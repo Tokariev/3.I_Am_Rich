a8999c38421886356d9e4095d1068a91fe9c5166
mykola <mykola.tokariev@gmail.com>
2020-11-06 22:56:38 -0100

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => MaterialApp();
+void main() => MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    );
